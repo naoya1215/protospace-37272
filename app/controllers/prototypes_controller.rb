0495827a69d3eacb08e917f3ff53f9bc1a431a1c
naoya1215 <llobomit@icloud.com>
2022-03-11 00:55:25 +0900

    new→create

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -11,7 +11,7 @@ class PrototypesController < ApplicationController
   end
   
   def create
-    @prototype = Prototype.new(prototype_params)
+    @prototype = Prototype.create(prototype_params)
     if @prototype.save
       redirect_to root_path
     else
