9506dd8808c816915c17ded1140de911964ba4a2
naoya1215 <llobomit@icloud.com>
2022-03-11 19:38:30 +0900

    updateアクションの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,7 +34,6 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
