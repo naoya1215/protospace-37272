083c58b38980a004ae507497fb003d16a3282e51
naoya1215 <llobomit@icloud.com>
2022-03-12 11:18:07 +0900

    修正12-1

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,17 +1,15 @@
 class PrototypesController < ApplicationController
   # exceptを使用(index, showは除きユーザーがログインしているかを確認する)
-  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
-    @prototypes = Prototype.includes(:user)
+    @prototype = Prototype.all
   end
 
   def new
     @prototype = Prototype.new
   end
-
+  
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
@@ -22,27 +20,31 @@ class PrototypesController < ApplicationController
   end
 
   def show
+    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments
+    @comments = @prototype.comments.includes(:user)
+
   end
 
   def edit
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
     if @prototype.update(prototype_params)
-      redirect_to prototype_path(@prototype)
+      redirect_to prototype_path
     else
       render :edit
     end
   end
 
   def destroy
-    if @prototype.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
   end
 
   private
@@ -51,11 +53,4 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def set_prototype
-    @prototype = Prototype.find(params[:id])
-  end
-
-  def contributor_confirmation
-    redirect_to root_path unless current_user == @prototype.user
-  end
 end
